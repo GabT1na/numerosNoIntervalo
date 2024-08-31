07e575878994617578c4661f04d1ebcb753ed7af
Gabrielly Cristina <167761882+GabT1na@users.noreply.github.com>
2024-08-31 22:54:28 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+// um programa que leia um valor inteiro N. Este valor sera a quantidade de valores inteiros x que serao lidos em seguida. Mostre quantos destes valores x estão dentro do intervalo [10, 20] e quantos estao fora do intervalo, mostrando essas informações;
+
+    int quantidadeDeNumerosDigitados = sc.nextInt();
+    int valoresDentroDoIntervalo = 0;
+    int valoresForaDoIntervalo = 0;
+    int numeroDigitado;
+
+    for(int i = 0; i< quantidadeDeNumerosDigitados; i++){
+      System.out.println("Digite o valor a ser comparado");
+      numeroDigitado = sc.nextInt();
+      if(numeroDigitado >= 10 && numeroDigitado <= 20){
+        valoresDentroDoIntervalo++;
+      }else{
+        valoresForaDoIntervalo++;
+      }
+    }
+    System.out.println ("Dentro do intervalo temos: " + valoresDentroDoIntervalo);
+    System.out.println ("Fora do intervalo temos: " + valoresForaDoIntervalo);
+    sc.close();
+  }
+
+
+}
